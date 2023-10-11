7139fd35bc067713253f4fa20e21d6c56038ff6b
Vaibhavi Chavhan <106610366+chikuu1721@users.noreply.github.com>
2023-10-11 22:50:00 +0530

    Add files via upload

---
+++ core-java/daytwo/DecisionMakingWithOperators.java
@@ -0,0 +1,25 @@
+package com.tns.daytwo;
+
+public class DecisionMakingWithOperators {
+
+	public static void main(String[] args) {
+		int x=5, y=7, a=10, b=11;
+		
+		if (x>=y) {
+			System.out.println("true");
+			
+		}else {
+			System.out.println("False");
+			
+		}
+		
+		if (!(a<b)||(a==b)) {
+			System.out.println("Condition is True");
+			
+		}else {
+			System.out.println("Condition is Flase");
+		}
+
+	}
+
+}
---
+++ core-java/daytwo/IfDemo.java
@@ -0,0 +1,16 @@
+package com.tns.daytwo;
+
+public class IfDemo {
+
+	public static void main(String[] args) {
+		
+		int x=5;
+		if(x!=5) {
+			System.out.println("Value of X is not 5");
+			
+		}
+		System.out.println("Value of X is 5");
+
+	}
+
+}
